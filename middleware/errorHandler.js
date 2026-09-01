babe697787b3e7d1c922940db73747525ff6072a
Nyolmo <nyolmorodney@gmail.com>
2026-09-01 12:00:17 +0300

    refactor(error-handler): add Winston logging and Axios error detection

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,26 +1,39 @@
+import logger from '../utils/logger.js';
+
 function asyncHandler(fn){
     return (req,res, next)=> Promise.resolve(fn(req,res,next)).catch(next);
 }
 
 function errorHandler( err, req, res, next){
-    console.error(`[error] ${req.method} ${req.originalUrl}`, err.message);
-
-    if(err.response){
-        return res.status(err.response.status).json({
-            error: 'Upstream API error',
-            status: err.response.status
-        });
-    }
-
-    if(err.code === 'ECONNABORTED'){
-        return res.status(504).json({
-            error: 'Upstream request timed out...'
-        });
-    }
-
-    return res.status(500).json({
-        error:'Internal middleware error'
+    logger.error({
+        message: err.message,
+        stack: err.stack,
+        method: req.method,
+        url: req.originalUrl
     });
+
+    if(axios.isAxiosError(err)){
+        if(err.response){
+            return res.status(err.response.status).json({
+                error: 'Upstream API error',
+                status: err.response.status
+        });
+    }
+
+        if(err.code === 'ECONNABORTED'){
+            return res.status(504).json({
+                error: 'Upstream request timed out...'
+            });
+        }
+
+
+
+    }
+    return res.status(500).json({
+            error:'Internal middleware error'
+        });
+
+
 };
 
 export {asyncHandler, errorHandler};
